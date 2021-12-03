ccf63aa8bafbb8c0ab47abdcd6e3f268746d0552
maghe <lorenzo.magherini.00@gmail.com>
2021-12-03 23:54:06 +0100

    Optimizing... 	modificato:             rrextab.cpp

--- rrextab.cpp
+++ rrextab.cpp
@@ -42,53 +42,54 @@ struct circ_buf_t
 	void push_back(int k)
 	{
 		buf[e] = k;
-		e++;
-		if ( e == sz )
-			e = 0;
+		e++; e &= 0x3ff;
+		/*if ( e == sz )
+			e = 0;*/
 		sz_++;
 	}
 	int pop_back()
 	{
 		int k = buf[e];
-		if( e == 0 )
-			e=sz-1;
-		else
-			e--;
+		/*if( e == 0 )
+			e=sz-1;*/
+		//else
+			e--; e &= 0x3ff;
 		sz_--;
 		return k;
 	}
 	void push_head(int k)
 	{
 		buf[s] = k;
-		if( s == 0 )
-			s=sz-1;
-		else
-			s--;
+		//if( s == 0 )
+		//	s=sz-1;
+		//else
+			s--; s &= 0x3ff;
 		sz_++;
 	}
 	int pop_head()
 	{
 		int k = buf[s];
-		s++;
-		if( s == sz )
-			s = 0;
+		s++; s &= 0x3ff;
+		//if( s == sz )
+		//	s = 0;
 		sz_--;
 		return k;
 	}
 	bool is_empty()
-	{ return ( e == s+1 || ( e == 0 && s==sz-1 ) ); }
+	{ return e == (s+1 & 0x3ff);/*( e == s+1 || ( e == 0 && s==sz-1 ) );*/ }
 	bool is_full()
 	{ return ( s == e ); }
 	int back()
 	{
-		if( e == 0 )
+		/*if( e == 0 )
 			return buf[sz-1];
 		else
-			return buf[e-1];
+			return buf[e-1];*/
+		return buf[e-1 & 0x3ff];
 	}
 	int operator[](size_t i)
 	{
-		return s+1+i >= sz ? buf[s+1+i-sz] : buf[s+1+i];
+		return s+1+i & 0x3ff; /*s+1+i >= sz ? buf[s+1+i-sz] : buf[s+1+i];*/
 	}
 	size_t size()
 	{ return sz_; }
@@ -163,17 +164,21 @@ struct match_shared_t
 	std::istream *is;	
 };
 
-void match(match_shared_t &m, rrex_tree *next, int reduce, int len=0, int idx=0 )
+void match(match_shared_t &m, rrex_tree *next, int reduce, int idx=0 )
 {
 
 	//if( next != nullptr )
 	//{
 		//read from tape:
+	match_start:
+
 		int c;
 		if( reduce >= 0 )
 			c = reduce;
 		else if( idx == m.buf->size() )
 		{
+			if( m.buf->is_full() )
+				exit(EXIT_FAILURE);
 			m.buf->push_back(m.is->get());
 			c = m.buf->back();
 		}
@@ -186,20 +191,39 @@ void match(match_shared_t &m, rrex_tree *next, int reduce, int len=0, int idx=0 
 			if( it->first.a <= c && c <= it->first.b )
 				to_check++;
 
-		for( auto it = next->lower_bound({c,c}); it != next->end(); it++ )
+		for( auto it = next->lower_bound({c,c}); it != next->end() && to_check; it++ )
 		{
 			if( it->first.a <= c && c <= it->first.b )
 			{
 				if( it->second.next != nullptr )
-					match(m, it->second.next, -1, 0, idx+1 );
-				if( it->second.reduce >= 0 )
 				{
+					if( to_check == 1 )
+					{
+						next = it->second.next;
+						reduce = -1;
+						idx++;
+						goto match_start;
+					}
+					std::cout << "{ ";
+					match(m, it->second.next, -1, idx+1 );
+					std::cout << "} ";
+				}
+				if( it->second.reduce >= 0 )
+				{	
 					if( idx > m.ret[0] )
 					{
 						m.ret[0] = idx;
 						m.ret[1] = it->second.reduce;
-					}	
-					match(m, m.root, it->second.reduce, 0, idx );
+					}
+					if( to_check == 1 )
+					{
+						next = m.root;
+						reduce = it->second.reduce;
+						goto match_start;
+					}
+					std::cout << "{ ";
+					match(m, m.root, it->second.reduce, idx );
+					std::cout << "} ";
 				}
 				to_check--;
 			}
