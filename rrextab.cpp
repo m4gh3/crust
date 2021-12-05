90a463c2b8f476079bf9d466cf87b369bf8a1205
maghe <lorenzo.magherini.00@gmail.com>
2021-12-05 23:28:07 +0100

    mmh lol 	modificato:             rrextab.cpp

--- rrextab.cpp
+++ rrextab.cpp
@@ -44,53 +44,39 @@ struct circ_buf_t
 	{
 		buf[e] = k;
 		e++; e &= 0x3ff;
-		/*if ( e == sz )
-			e = 0;*/
 		sz_++;
 	}
 	int pop_back()
 	{
 		int k = buf[e];
-		/*if( e == 0 )
-			e=sz-1;*/
-		//else
-			e--; e &= 0x3ff;
+		e--; e &= 0x3ff;
 		sz_--;
 		return k;
 	}
 	void push_head(int k)
 	{
 		buf[s] = k;
-		//if( s == 0 )
-		//	s=sz-1;
-		//else
-			s--; s &= 0x3ff;
+		s--; s &= 0x3ff;
 		sz_++;
 	}
-	int pop_head()
+	int pop_head(int n)
 	{
 		int k = buf[s];
-		s++; s &= 0x3ff;
-		//if( s == sz )
-		//	s = 0;
-		sz_--;
+		s+=n; s &= 0x3ff;
+		sz_-=n;
 		return k;
 	}
 	bool is_empty()
-	{ return e == (s+1 & 0x3ff);/*( e == s+1 || ( e == 0 && s==sz-1 ) );*/ }
+	{ return e == (s+1 & 0x3ff); }
 	bool is_full()
 	{ return ( s == e ); }
 	int back()
 	{
-		/*if( e == 0 )
-			return buf[sz-1];
-		else
-			return buf[e-1];*/
 		return buf[e-1 & 0x3ff];
 	}
 	int operator[](size_t i)
 	{
-		return buf[s+1+i & 0x3ff]; /*s+1+i >= sz ? buf[s+1+i-sz] : buf[s+1+i];*/
+		return buf[s+1+i & 0x3ff];
 	}
 	size_t size()
 	{ return sz_; }
@@ -141,7 +127,7 @@ int rrex_tree_size(rrex_tree *root)
 // range_beg, range_end, next_offset, reduce_to
 // 32bit, 32bit, 32bit, 32bit
 
-int flatten_tree(rrex_tree *root, int *buf )
+/*int flatten_tree(rrex_tree *root, int *buf )
 {
 	if( root == nullptr )
 		return 0;
@@ -152,7 +138,7 @@ int flatten_tree(rrex_tree *root, int *buf )
 	{
 		
 	}
-}
+}*/
 
 int rrex_insert(std::vector<rrex_key> &&rrex, int64_t reduce=0 )
 { return rrex_insert(rrex, reduce ); }
@@ -166,7 +152,11 @@ struct match_shared_t
 	void *(**lang_callbacks)(match_shared_t &, void *, void *);
 };
 
-void *match(match_shared_t &m, rrex_tree *next, int64_t reduce, int idx=0, int nonterminals=0 )
+#define DO_CALLBACK 	0x40000000 
+#define DO_RECURSION 	0x20000000
+#define REDMASK		0x1fffffff
+
+void *match(match_shared_t &m, rrex_tree *next, int64_t reduce, int idx=0, int mlen=0, int nonterminals=0 )
 {
 	//if( next != nullptr )
 	//{
@@ -174,7 +164,7 @@ void *match(match_shared_t &m, rrex_tree *next, int64_t reduce, int idx=0, int n
 	void *pval = NULL, *sval = NULL;
 	match_start:
 
-		int c;
+		int64_t c;
 		if( reduce >= 0 )
 			c = reduce;
 		else if( idx == m.buf->size() )
@@ -184,11 +174,13 @@ void *match(match_shared_t &m, rrex_tree *next, int64_t reduce, int idx=0, int n
 			m.buf->push_back(m.is->get());
 			c = m.buf->back();
 			idx++;
+			mlen++;
 		}
 		else
-			c = (*m.buf)[idx++];	
-		
-		std::cout << '(' << c << ',' << idx << ',' << m.buf->size() << ')';
+		{
+			c = (*m.buf)[idx++];
+			mlen++;
+		}
 
 		int to_check = 0;
 
@@ -202,113 +194,115 @@ void *match(match_shared_t &m, rrex_tree *next, int64_t reduce, int idx=0, int n
 			{
 				if( it->second.next != nullptr )
 				{
-					std::cout << 'n';
 					if( to_check == 1 && (it->second.reduce < 0) )
 					{
-						std::cout << 'o';
 						next = it->second.next;
 						reduce = -1;
-						//std::cout << '+';
-						//idx++;
 						goto match_start;
 					}
 					std::cout << "{ ";
-					match(m, it->second.next, -1, idx/*+1*/ );
+					match(m, it->second.next, -1, idx, mlen );
 					std::cout << "} ";
 				}
 				if( it->second.reduce >= 0 )
 				{
-					std::cout << 'r';	
-					if( idx > m.ret[0] )
+					if( mlen > m.ret[0] )
 					{
-						std::cout << 'm';
-						m.ret[0] = idx;
+						m.ret[0] = mlen;
 						m.ret[1] = it->second.reduce;
+						m.ret[2] = idx;
 						if( nonterminals == 2 )
 							nonterminals = 1;
 					}
 					if( to_check == 1 )
 					{
-						std::cout << 'o';
 						next = m.root;
 						reduce = it->second.reduce;
 						goto match_start;
 					}
 					std::cout << "{ ";
-					match(m, m.root, it->second.reduce, idx );
+					match(m, m.root, it->second.reduce, idx, mlen );
 					std::cout << "} ";
 				}
-				std::cout << '-';
 				to_check--;
 			}
 		}
-		/*if( nonterminals & (m.ret[1] >= 0) )
+		if( nonterminals & (m.ret[1] >= 0) )
 		{
 			reduce = m.ret[1];
-			if( m.ret[1] & 0x2000000000000000 )
-				sval = match(m, m.root, m.ret[1], idx, true );
-			if( reduce & 0x4000000000000000 )
+			if( (reduce & DO_CALLBACK) && ( m.lang_callbacks[((reduce & REDMASK)-256<<1)+0] != NULL ) )
+				pval = (m.lang_callbacks[((reduce & REDMASK)-256<<1)+0])(m, pval, sval );
+			m.buf->pop_head(m.ret[2]);
+			idx = 0;
+			if( m.ret[1] & DO_RECURSION )
+				sval = match(m, m.root, m.ret[1] & REDMASK, 0, mlen, true );
+			if( (reduce & DO_CALLBACK) && ( m.lang_callbacks[((reduce & REDMASK)-256<<1)+1] != NULL ) )
 			{
 				//do callback
-				pval = (m.lang_callbacks[m.ret[1] & 0xbfffffffffffffff ])(m, pval, sval );
+				pval = (m.lang_callbacks[((reduce & REDMASK)-256<<1)+1])(m, pval, sval );
 				sval = NULL;
 			}
-			if( reduce != m.ret[1] )
+			if( reduce != (m.ret[1] & REDMASK) )
 			{
-				reduce = m.ret[1];
+				reduce = m.ret[1] & REDMASK;
+				next = m.root; //?
 				nonterminals = 2;
 				goto match_start;
 			}
-		}*/
+		}
 
 	return pval;
 	//}
 
 }
 
-// p -> p*n
-// p -> n
-// s -> s+p //now the problem is how you actually forbid that rule from happening?
-// s -> p   // see? you don't want to apply that rule after s+ right?
-
 void *print_id(match_shared_t &m, void *pval, void *sval )
 {
-	std::cout << (*m.buf)[0] << std::endl;
+	std::cout << "identfier: ";
+	for(int i=0; i < m.ret[2]; i++ )
+		std::cout << (char)(*m.buf)[i];
+	std::cout << std::endl;
 	return NULL;
+}
+
+void *make_num(match_shared_t &m, void *pval, void *sval )
+{
+	int *retval = new int(0);
+	for(int i=0; i < m.ret[2]; i++ )
+		((*retval) *= 10)+=((*m.buf)[i]-'0');
+	std::cout << (*retval) << std::endl;
+	return retval;
+}
+
+void *make_sum(match_shared_t &m, void *pval, void *sval ) 
+{
+	(*(int *)pval) += (*(int *)sval);
+	std::cout << (*(int *)pval) << std::endl;
+	return pval;
 }
 
 void match(int64_t *ret, rrex_tree *root, rrex_tree *next, circ_buf_t &buf, std::istream &is, int64_t reduce=-1, int idx=0 )
 {
-	void *(*lang_callbacks[])(match_shared_t &, void *, void * ) = {print_id};
+	void *(*lang_callbacks[])(match_shared_t &, void *, void * ) = {NULL, NULL, make_num, NULL, NULL, make_sum };
 	match_shared_t m{ret, root, &buf, &is, lang_callbacks };
-	match(m, root, reduce, idx );
+	match(m, root, reduce, idx, 0, 1 );
 }
 
-
-#define DO_CALLBACK 0x4000000000000000 
-#define DO_RECURSION 0x2000000000000000
-
 //#define ID (DO_CALLBACK | 0)
-#define ID 256
+//#define ID 256
+#define START 256
+#define NUM 257
+#define SUM 258
 
 int main()
 {
-	rrex_insert({{'_','_'}}, ID );
-	rrex_insert({{'a','z'}}, ID );
-	rrex_insert({{'A','Z'}}, ID );
-	rrex_insert({{ID, ID}, {'a','z'}}, ID );
-	rrex_insert({{ID, ID}, {'A','Z'}}, ID );
-	rrex_insert({{ID, ID}, {'0','9'}}, ID );
-	rrex_insert({{ID, ID}, {'_','_'}}, ID );
-	rrex_insert({{'u','u'}, {'v','v'}, {'a','a'}, {'l','l'}, {' ', ' '}}, 257 );
-	rrex_insert({{'r','r'}, {'e','e'}, {'f','f'}, {' ',' '}}, 258 );
-	rrex_insert({{'s','s'}, {'t','t'}, {'r','r'}, {'u','u'}, {'c','c'}, {'t','t'}, {' ',' '}}, 259 );
-	rrex_insert({{'u','u'}, {'n','n'}, {'i','i'}, {'o','o'}, {'n','n'}, {' ',' '}}, 260 );
-	rrex_insert({{'e','e'}, {'n','n'}, {'u','u'}, {'m','m'}, {' ',' '}}, 261 );
-	rrex_insert({{'c','c'}, {'o','o'}, {'n','n'}, {'s','s'}, {'t','t'}, {' ',' '}}, 262 );
+	rrex_insert({{START,START}, {'0','9'}}, DO_CALLBACK | NUM );
+	rrex_insert({{DO_CALLBACK | NUM, DO_CALLBACK | NUM}, {'0','9'}}, DO_CALLBACK | NUM );
+	rrex_insert({{NUM,SUM}, {'+','+'} }, DO_RECURSION | START );
+	rrex_insert({{NUM,NUM}, {'+','+'}, {NUM,NUM}}, DO_RECURSION | SUM );
 	std::cout << "rrex_tree sz:" << rrex_tree_size(rrex_main_tree_ptr) << std::endl;
-	int64_t ret[2]={-1,-1};
-	match(ret, rrex_main_tree_ptr, rrex_main_tree_ptr, matchbuf, std::cin );
+	int64_t ret[3]={-1,-1,-1};
+	match(ret, rrex_main_tree_ptr, rrex_main_tree_ptr, matchbuf, std::cin, START );
 	std::cout << "\nlen:" << ret[0] << ", reduce:" << ret[1] << std::endl;
 	std::cout << std::endl;
 	return 0;
