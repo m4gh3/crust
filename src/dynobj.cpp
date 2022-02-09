1d8e2eb0a9e81b1c25ff7d010285299f92006ae5
maghe <lorenzo.magherini.00@gmail.com>
2022-02-09 23:41:47 +0100

    added index by num

--- src/dynobj.cpp
+++ src/dynobj.cpp
@@ -1,5 +1,7 @@
 #include <string>
+
 #include <unordered_map>
+#include <map>
 
 #include <iostream>
 
@@ -18,7 +20,8 @@ class dyn_obj
 
 		dyn_obj()
 		{
-			value.members = new std::unordered_map<std::string, dyn_obj >;
+			value.obj.by_str = new std::unordered_map<std::string, dyn_obj >;
+			value.obj.by_num = new std::map<int, dyn_obj >;
 			type = dyn_obj_type::OBJ;
 		}
 		
@@ -48,9 +51,11 @@ class dyn_obj
 			{
 				case dyn_obj_type::OBJ:
 					//deep copy
-					value.members = new std::unordered_map<std::string, dyn_obj >;
-					for( auto &pair : *dobj.value.members )
-						(*value.members)[pair.first] = pair.second;
+					value.obj.by_str = new std::unordered_map<std::string, dyn_obj >;
+					for( auto &pair : *dobj.value.obj.by_str )
+						(*value.obj.by_str)[pair.first] = pair.second;
+					for( auto &pair : *dobj.value.obj.by_num )
+						(*value.obj.by_num)[pair.first] = pair.second;
 					break;
 				case dyn_obj_type::DYN_PTR:
 					value.ptr = dobj.value.ptr;
@@ -77,7 +82,7 @@ class dyn_obj
 		~dyn_obj()
 		{
 			if( type == dyn_obj_type::OBJ )
-				delete value.members;
+				delete value.obj.by_str;
 			if( type == dyn_obj_type::STR )
 				value.str.std::string::~string();
 		}
@@ -120,7 +125,10 @@ class dyn_obj
 		}
 	
 		dyn_obj &operator[](const std::string &key)
-		{ return (*value.members)[key]; }
+		{ return (*value.obj.by_str)[key]; }
+
+		dyn_obj &operator[](const int key)
+		{ return (*value.obj.by_num)[key]; }
 
 	private:
 
@@ -128,7 +136,11 @@ class dyn_obj
 		{
 			value_union() {}
 			~value_union(){}
-			std::unordered_map<std::string, dyn_obj > *members;
+			struct
+			{
+				std::unordered_map<std::string, dyn_obj > *by_str;
+				std::map<int, dyn_obj > *by_num;
+			} obj;
 			dyn_obj *ptr;
 			int i;
 			std::string str;
@@ -142,6 +154,7 @@ int main()
 	dyn_obj subobj; subobj = new dyn_obj;
 	(*subobj.get_ptr())["birth_year"] = 2000;
 	(*subobj.get_ptr())["continent"] = "Europe";
+	(*subobj.get_ptr())[0] = "Lol";
 	d["name"] = "m4gh3";
 	d["info"] = subobj;
 	std::cout << d["name"].get_str() << std::endl;
